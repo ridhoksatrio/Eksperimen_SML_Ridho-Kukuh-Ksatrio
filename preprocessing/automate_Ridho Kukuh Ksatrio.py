47ada546220d308fb7bef6bbdb6f4d8a6917ea7b
ridhoksatrio <ridhoksatrio@gmail.com>
2025-12-22 08:00:43 +0700

    Update automate_Ridho Kukuh Ksatrio.py

--- preprocessing/automate_Ridho Kukuh Ksatrio.py
+++ preprocessing/automate_Ridho Kukuh Ksatrio.py
@@ -1,49 +1,62 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import os
 
 def run_preprocessing(input_path, output_folder):
-    # Cek apakah file input ada di root
     if not os.path.exists(input_path):
         print(f"Error: {input_path} tidak ditemukan!")
         return
 
     df = pd.read_csv(input_path)
+    df = df.drop_duplicates()
     df = df.dropna()
 
     def categorize_popularity(score):
         if score <= 33: return 0
         elif score <= 66: return 1
         else: return 2
-
     df['pop_category'] = df['popularity'].apply(categorize_popularity)
+
 
     cols_to_drop = ['track_id', 'track_name', 'artist', 'album', 'release_date', 'popularity']
     df_model = df.drop(cols_to_drop, axis=1)
 
+    numeric_cols = df_model.select_dtypes(include=[np.number]).columns
+    for col in numeric_cols:
+        if col != 'pop_category':
+            lower_limit = df_model[col].quantile(0.01)
+            upper_limit = df_model[col].quantile(0.99)
+            df_model[col] = np.clip(df_model[col], lower_limit, upper_limit)
+
     X = df_model.drop('pop_category', axis=1)
     y = df_model['pop_category']
-
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
-    cleaned_df = pd.DataFrame(X_train_scaled, columns=X.columns)
-    cleaned_df['pop_category'] = y_train.values
 
-    # Buat folder jika belum ada
+    train_df = pd.DataFrame(X_train_scaled, columns=X.columns)
+    train_df['pop_category'] = y_train.values
+    train_df['set_type'] = 'train'
+
+    test_df = pd.DataFrame(X_test_scaled, columns=X.columns)
+    test_df['pop_category'] = y_test.values
+    test_df['set_type'] = 'test'
+
+    final_df = pd.concat([train_df, test_df], axis=0)
+
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-
-    # Simpan dengan nama yang konsisten untuk YAML
+        
     output_path = os.path.join(output_folder, 'spotify_preprocessing.csv')
-    cleaned_df.to_csv(output_path, index=False)
-    print(f"Berhasil! Data disimpan di: {output_path}")
+    final_df.to_csv(output_path, index=False)
+    print(f"Berhasil! Hasil Preprocessing (Train & Test) disimpan di: {output_path}")
 
 if __name__ == "__main__":
-    # Path ini harus sesuai dengan struktur folder di GitHub
     run_preprocessing('spotify_analysis_dataset.csv', 'preprocessing')
